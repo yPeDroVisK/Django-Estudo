e9b41c2ff06d263fc37eaef238de87329f4a6d39
yPeDroVisK <138019066+yPeDroVisK@users.noreply.github.com>
2026-09-25 12:00:59 -0300

    Add: Models db
    Criação do models para o banco de dados

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
